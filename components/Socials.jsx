2cbb04aca5e70a7edc140cfc4707b3887dab41ba
treyisraeliii <wisrael@clemson.edu>
2024-09-06 09:08:24 -0400

    base layout

---
+++ components/Socials.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaInstagram />, path: "" },
+];
+
+const Socials = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Socials;
